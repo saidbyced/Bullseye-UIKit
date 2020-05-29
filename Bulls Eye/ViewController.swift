7706ce8e28c9e8ba2376fe1e13e6d7a50d9738a4
Chris Eadie <chris.eadie@outlook.com>
2020-05-29 15:39:36 +0100

    Add score calculation and link to Hit Me button

--- Bulls Eye/ViewController.swift
+++ Bulls Eye/ViewController.swift
@@ -9,18 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var target: Int = 100
+    var sliderValue: Int = 50
+    var totalScore: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        randomiseTarget()
     }
 
-    @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Button pressed", message: "You pressed me.", preferredStyle: .alert)
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        sliderValue = Int(round(slider.value))
+    }
+    
+    @IBAction func hitMeButtonPressed() {
+        
+        let scoreForThisRound = 100 - abs(target - sliderValue)
+        
+        let alert = UIAlertController(title: "Button pressed", message: "Slider value is \(sliderValue)\nYou scored \(scoreForThisRound) points this round.", preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        print("Hit me")
+        
+        addToTotalScore(scoreForThisRound)
+        randomiseTarget()
     }
-
+    
+    @IBAction func startOverButtonPressed() {
+        print("Start over")
+    }
+    
+    func randomiseTarget() {
+        target = Int.random(in: 1...100)
+    }
+    
+    func addToTotalScore(_ scoreForThisRound: Int){
+        totalScore += scoreForThisRound
+    }
+    
 }
