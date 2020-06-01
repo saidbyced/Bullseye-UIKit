68c5213eb4ee8a1068d0fd8f59a8510c5227884c
Chris Eadie <chris.eadie@outlook.com>
2020-06-01 13:08:55 +0100

    Reset to retry creation

--- Bulls Eye/ViewController.swift
+++ Bulls Eye/ViewController.swift
@@ -2,54 +2,42 @@
 //  ViewController.swift
 //  Bulls Eye
 //
-//  Created by Chris Eadie on 27/05/2020.
+//  Created by Chris Eadie on 01/06/2020.
 //  Copyright © 2020 ChrisEadieDesigns. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var target: Int = 100
-    var sliderValue: Int = 50
-    var totalScore: Int = 0
 
+    var targetValue = Int.random(in: 1...100)
+    let totalScore = 0
+    let currentRound = 1
+    let roundedSliderValue = 50
+    
+    @IBOutlet weak var targetValueLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var totalScoreLabel: UILabel!
+    @IBOutlet weak var currentRoundLabel: UILabel!
+    
+    @IBAction func sliderMoved() {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        randomiseTarget()
-    }
 
-    @IBAction func sliderMoved(_ slider: UISlider) {
-        sliderValue = Int(round(slider.value))
+        // Do any additional setup after loading the view.
     }
     
-    @IBAction func hitMeButtonPressed() {
-        
-        let scoreForThisRound = 100 - abs(target - sliderValue)
-        
-        let alert = UIAlertController(title: "Button pressed", message: "Slider value is \(sliderValue)\nYou scored \(scoreForThisRound) points this round.", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
-        
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-        print("Hit me")
-        
-        addToTotalScore(scoreForThisRound)
-        randomiseTarget()
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    @IBAction func startOverButtonPressed() {
-        print("Start over")
-    }
-    
-    func randomiseTarget() {
-        target = Int.random(in: 1...100)
-    }
-    
-    func addToTotalScore(_ scoreForThisRound: Int){
-        totalScore += scoreForThisRound
-    }
-    
+    */
+
 }
