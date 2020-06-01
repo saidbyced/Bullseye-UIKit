3b45e2a0b304fb6dd4d6e5c1b01db2bfcd1a4018
Chris Eadie <chris.eadie@outlook.com>
2020-06-01 13:19:42 +0100

    Add IBActions

--- Bulls Eye/ViewController.swift
+++ Bulls Eye/ViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
     var targetValue = Int.random(in: 1...100)
     var roundedSliderValue = 0
     var totalScore = 0
     var currentRound = 1
     
+    
+    // MARK: IBOutlets
+    
     @IBOutlet weak var targetValueLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var totalScoreLabel: UILabel!
     @IBOutlet weak var currentRoundLabel: UILabel!
+
+    
+    // MARK: Override Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +34,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
+    // MARK: IBActions
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        roundedSliderValue = Int(slider.value.rounded())
+    }
+    
+    @IBAction func hitMeButtonPressed() {
+        print("Hit Me!")
+    }
+    
+    @IBAction func startOverButtonPressed() {
+        print("Start Over")
+    }
+    
     /*
     // MARK: - Navigation
 
